95c1258dd629dc97915155d4f0c3ff44fee35ee3
mardanius <mariojeda.mg@gmail.com>
2022-07-29 10:03:23 -0400

    Added 'Styling Elements' and 'Creating Elements'

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-// DOM Manipulation
+/*=========================
+JavaScript DOM Manipulation
+===========================
+
+/*=======
+SELECTORS
+=========
 
 // Get element by id
 
@@ -24,4 +30,37 @@ console.log(container)
 // QuerySelectorAll
 
 const containers = document.querySelectorAll('div')
-console.log(containers)
+console.log(containers) */
+
+/*==============
+STYLING ELEMENTS
+================
+
+// Changing the color of the title
+const title = document.querySelector('#main-heading');
+
+title.style.color = "red";
+
+// Chaning the size of the list items
+
+const list_items = document.querySelectorAll('.list-items');
+
+// Note, in JS we use camel notation: fontSize instead of font-size.
+//list_items.style.fontSize = '100px';
+
+for (let i = 0; i < list_items.length; i++) {
+    list_items[i].style.fontSize = '30px';
+}
+*/
+
+/*===============
+CREATING ELEMENTS
+================*/
+
+// const ul = document.querySelector('ul');
+// const li = document.createElement('li');
+
+// ul.append(li);
+
+// li.innerText = 'Burt Munro, un suenho una leyenda' // Recommended
+// li.innerHTML = 'Burt Munro, un suenho una leyenda' // Not recommended for security.
